f812e332b9a4f58a2a256414d1028c7a38259338
flow2708 <157009478+flow2708@users.noreply.github.com>
2026-02-10 23:44:50 +0300

    Update CatNotFoundException.java

--- src/main/java/com/example/demo/exceptions/CatNotFoundException.java
+++ src/main/java/com/example/demo/exceptions/CatNotFoundException.java
@@ -1,5 +1,9 @@
 package com.example.demo.exceptions;
 
+/**
+ *     Без обработчика: Spring вернет статус 500 (Internal Server Error) с техническим стектрейсом.
+ *     С этим обработчиком: Вернется статус 404 (NOT_FOUND) с понятным сообщением: "Кот с id 5 не найден".
+ */
 public class CatNotFoundException extends RuntimeException {
     public CatNotFoundException(Long id) {
         super("Сouldn't find the cat with ID number " + id);
