6f49dc888737bfcc5452d226c9030f03d8b43266
flow2708 <157009478+flow2708@users.noreply.github.com>
2026-03-03 20:25:15 +0300

    Update CurrentTimeShedule.java

--- src/main/java/com/example/demo/CurrentTimeShedule.java
+++ src/main/java/com/example/demo/CurrentTimeShedule.java
@@ -10,7 +10,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Component
-@EnableScheduling
+//@EnableScheduling включить для активации задачи
 public class CurrentTimeShedule {
     private static final Logger log = LoggerFactory.getLogger(DeleteShedule.class);
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
