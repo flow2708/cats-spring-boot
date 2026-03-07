3a2faa0ddf29dc68b78e5f0b4ea6d248408f7f83
flow2708 <157009478+flow2708@users.noreply.github.com>
2026-03-07 11:29:09 +0300

    Update User.java

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -40,18 +40,12 @@ public class User {
         this.password = password;
     }
 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public Role getRole() {
-        return role;
-    }
+    public Role getRole() { return role; }
+    public void setRole(Role role) { this.role = role; }
 
-    // Геттеры и сеттеры
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
 
